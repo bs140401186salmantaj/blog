dc2d0fa77b7e0da340204e2f575bbc0573cb6986
bs140401186 <bs140401186@vu.edu.pk>
2021-07-24 02:10:03 +0500

    Rout waild card constraints

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ Route::get('posts/{post}', function ($slug) {
    
          $path = __DIR__ . "/../resources/posts/{$slug}.html";
 
+         ddd($path);
+
          if(! file_exists($path)){
              //dd('file dose not exist');
              return redirect('/');
@@ -32,4 +34,4 @@ Route::get('posts/{post}', function ($slug) {
      return view('post',[
          'post'=>$post
      ]);
-});
+})->where('post','[A-z_/-]+');
