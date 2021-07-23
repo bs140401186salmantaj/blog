2e78ab988240ae6b5f0e23f8e5e36bfdce0c95bf
bs140401186 <bs140401186@vu.edu.pk>
2021-07-24 00:54:11 +0500

    post page and post detail page done static

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,57 @@
+<html>
+
+<head>
+
+    <link rel="stylesheet" href="/app.css">
+    <title>My  Blog</title>
+    
+</head>
+<body>
+
+<article>
+
+    <h1><a href="/post">My first Blog</a></h1>
+
+    <p>
+        Today, there are many dummy/filler text generators to choose from,
+         so you don’t necessarily need to use Lorem ipsum. There’s, for example, 
+         Corporate ipsum which is based off of business terminologies. 
+        Or Cupcake ipsum for those who love baked goods.
+    </p>
+
+
+</article>
+
+
+<article>
+
+    <h1><a href="/post">My Second Blog</a></h1>
+
+    <p>
+        Today, there are many dummy/filler text generators to choose from,
+         so you don’t necessarily need to use Lorem ipsum. There’s, for example, 
+         Corporate ipsum which is based off of business terminologies. 
+        Or Cupcake ipsum for those who love baked goods.
+    </p>
+
+
+</article>
+
+<article>
+
+    <h1><a href="/post">My Third Blog</a></h1>
+
+    <p>
+        Today, there are many dummy/filler text generators to choose from,
+         so you don’t necessarily need to use Lorem ipsum. There’s, for example, 
+         Corporate ipsum which is based off of business terminologies. 
+        Or Cupcake ipsum for those who love baked goods.
+    </p>
+
+
+</article>
+
+
+</body>
+
+</html>
